98aa69cbb0efeee87b1040cd3ab8999da3411578
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2017-12-29 19:24:20 +0530

    ready

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v4!zn4yt%f2_4lq42%=4+h5=m=%c#v-sqav3qi6_$_b9x#33kz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["instaapplication.herokuapp.com"]
 
 
 # Application definition
