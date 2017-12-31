f38669fb33ee460877d24dccad4bc52b4eaa6661
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2017-12-31 16:40:06 +0530

    debug turned off

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v4!zn4yt%f2_4lq42%=4+h5=m=%c#v-sqav3qi6_$_b9x#33kz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =False
 
 ALLOWED_HOSTS = ['instaapplication.herokuapp.com','127.0.0.1']
 
